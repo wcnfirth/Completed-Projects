834b3b490967ba68b162264ff786352b0b8e104b
Will Firth <firth008@umn.edu>
2020-03-09 17:17:29 -0500

    Updating names of current directories - removing old files

--- CommandLineShell/test_args.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]){
-  printf("%d args received\n",argc);
-  for(int i=0; i<argc; i++){
-    printf("%d: %s\n",i,argv[i]);
-  }
-  return argc;
-}
